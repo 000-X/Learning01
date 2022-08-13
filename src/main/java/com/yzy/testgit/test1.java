9ba784e34729d1b0649c362ae0788bf6e0bdf9bf
Softrid <1595349991@qq.com>
2022-08-13 19:13:57 +0800

    添加test1

--- src/main/java/com/yzy/testgit/test1.java
+++ src/main/java/com/yzy/testgit/test1.java
@@ -3,5 +3,6 @@ package com.yzy.testgit;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("你好，git");
+        System.out.println("你好，分支main");
     }
 }
