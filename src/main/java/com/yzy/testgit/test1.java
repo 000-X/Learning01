ae155bacade0d9afb4c4ea61a578ab3bcf8304c5
Softrid <1595349991@qq.com>
2022-08-13 19:49:58 +0800

    添加branch分支

--- src/main/java/com/yzy/testgit/test1.java
+++ src/main/java/com/yzy/testgit/test1.java
@@ -1,6 +1,9 @@
 package com.yzy.testgit;
 
 public class test1 {
+    public void branch(){
+        System.out.println("你好，我是branch");
+    }
     public static void main(String[] args) {
         System.out.println("你好，git");
         System.out.println("你好，分支main");
