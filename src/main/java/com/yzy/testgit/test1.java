f354bf3f8496f0cb45ed64fcf2c7a657dc1dde06
Softrid <1595349991@qq.com>
2022-08-13 22:10:35 +0800

    采用branch

--- src/main/java/com/yzy/testgit/test1.java
+++ src/main/java/com/yzy/testgit/test1.java
@@ -1,5 +1,8 @@
 package com.yzy.testgit;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class test1 {
     public void branch(){
         System.out.println("你好，我是branch");
@@ -8,5 +11,6 @@ public class test1 {
         System.out.println("你好，git");
         System.out.println("你好，分支main");
         new test1().branch();
+        Branch branch = new Branch();
     }
 }
